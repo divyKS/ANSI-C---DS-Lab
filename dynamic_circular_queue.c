939e341a1ff5494d971ff76c606ec80dd6bda03c
divyKS <divycodescompetes@gmail.com>
2026-08-13 16:42:15 +0530

    queue enqueue

--- dynamic_circular_queue.c
+++ dynamic_circular_queue.c
@@ -1,14 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MIN_CAPACITY 4
+
 typedef struct {
-    int front;
-    int back;
+    int front; // points to the first element
+    int rear; // points to the index where the next element will be enqueued
     int *data;
 
     int size;
     int CAPACITY;
 } Queue;
+
+int init(Queue *queue) {
+    int *temp = malloc(MIN_CAPACITY * sizeof(int));
+    if(temp == NULL) {
+        return 0;
+    }
+    queue->data = temp;
+    queue->front = -1;
+    queue->rear = 0;
+    queue->CAPACITY = MIN_CAPACITY;
+}
+
+int isEmpty(const Queue *queue) {
+    if(queue->size == 0) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+int isFull(const Queue *queue) {
+    if(queue->size == queue->CAPACITY) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+int enqueue(Queue *queue, int value) {
+    int i;
+    if(queue == NULL) {
+        return 0;
+    }
+    if(queue->size == queue->CAPACITY) {
+        // resize queue and copy and clean the ordering
+        int newCapacity = queue->CAPACITY * 2;
+        int *temp = malloc(newCapacity * sizeof(int));
+        if(temp == NULL) {
+            return 0;
+        }
+        for(i = 0; i < queue->size; i++) {
+            *(temp + i) = queue->data[(queue->front + i) % queue->CAPACITY];
+        }
+        free(queue->data);
+        queue->data = temp;
+        queue->front = 0;
+        queue->rear = queue->size;
+        queue->CAPACITY = newCapacity;
+    }
+
+    if(queue->front == -1) {
+        queue->front = 0; // re-intialize now
+    }
+    
+    queue->data[queue->rear] = value;
+    queue->rear = (queue->rear + 1) % queue->CAPACITY;
+    queue->size++;
+}
 
 int main(void) {
     return 0;
