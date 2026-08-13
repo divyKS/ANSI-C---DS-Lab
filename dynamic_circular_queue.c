287fa5665bf6b5a02a0a28cf798eafab5840ed34
divyKS <divycodescompetes@gmail.com>
2026-08-13 07:54:10 +0530

    queue struct

---
+++ dynamic_circular_queue.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int front;
+    int back;
+    int *data;
+
+    int size;
+    int CAPACITY;
+} Queue;
+
+int main(void) {
+    return 0;
+}
