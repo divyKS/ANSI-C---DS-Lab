791477e0839fa8959892c81c20299f0cb3677409
divyKS <divycodescompetes@gmail.com>
2026-08-14 11:46:18 +0530

    ll end

--- ll.c
+++ ll.c
@@ -32,7 +32,7 @@ int insertNode(LinkedList *list, int value) {
     }
     else {
         Node *temp = list->head;
-        
+
         while(temp != NULL && temp->next != NULL) {
             temp = temp->next;
         }
@@ -61,6 +61,128 @@ int insertNodeAtHead(LinkedList *list, int value) {
     return 1;
 }
 
+int insertAtIndex(LinkedList *list, int index, int value) {
+    if(list == NULL || index < 0 || index > list->size) {
+        return 0;
+    }
+
+    if(index == 0) {
+        return insertNodeAtHead(list, value);
+    }
+
+    Node *newNode = malloc(sizeof(Node));
+
+    if(newNode == NULL) {
+        return 0;
+    }
+
+    Node *temp = list->head;
+    int skips;
+    for(skips = 1; skips <= index - 1; skips++) {
+        temp = temp->next;
+    }
+
+    // temp now points to the node after which this new node is to be inserted
+    newNode->data = value;
+    newNode->next = temp->next;
+    temp->next = newNode;
+
+    list->size++;
+
+    return 1;
+}
+
+int deleteNodeAtHead(LinkedList *list, int *deletedNodeValue) {
+    if(list == NULL || list->head == NULL) {
+        return 0;
+    }
+
+    Node* temp = list->head;
+    *deletedNodeValue = temp->data;
+    list->head = list->head->next;
+    free(temp);
+    list->size--;
+
+    return 1;
+}
+
+int deleteLast(LinkedList *list, int *lastNodeValue) {
+    if (list->head == NULL) {
+        return 0;
+    }
+
+    if (list->head->next == NULL) {
+        *lastNodeValue = list->head->data;
+
+        free(list->head);
+
+        list->head = NULL;
+        list->size--;
+
+        return 1;
+    }
+
+    Node *temp = list->head;
+
+    while (temp != NULL && temp->next != NULL && temp->next->next != NULL) {
+        temp = temp->next;
+    }
+
+    Node *lastNode = temp->next;
+    *lastNodeValue = temp->data;
+    temp->next = NULL;
+    free(lastNode);
+
+    list->size--;
+
+    return 1;
+}
+
+int deleteNodeAtIndex(LinkedList *list, int index, int *deletedValue) {
+    if(list == NULL || index < 0 || index >= list->size) {
+        return 0;
+    }
+
+    if(index == 0) {
+        return deleteNodeAtHead(list, deletedValue);
+    }
+
+    Node *temp = list->head;
+    int skips;
+    for(skips = 1; skips <= index - 1; skips++) {
+        temp = temp->next;
+    }
+
+    Node *deletedNode = temp->next;
+    *deletedValue = deletedNode->data;
+    temp->next = deletedNode->next;
+    free(deletedNode);
+    list->size--;
+
+    return 1;
+}
+
+int updateValueAtIndex(LinkedList *list, int index, int value) {
+    if (index < 0 || index >= list->size) {
+        return 0;
+    }
+
+    Node *temp = list->head;
+
+    int i;
+    for (i = 0; i < index; i++) {
+        temp = temp->next;
+    }
+
+    temp->data = value;
+
+    return 1;
+}
+
+void freeList(LinkedList *list) {
+    
+}
+
 void sout(const LinkedList *list) {
     Node *temp = list->head;
     while(temp != NULL) {
@@ -77,5 +199,16 @@ int main(void) {
     init(&list);
     insertNodeAtHead(&list, 10);
     insertNode(&list, 20);
+    insertAtIndex(&list, 0, -10);
+    insertAtIndex(&list, 3, 30);
+    // if(insertAtIndex(&list, 10, 100) == 0) {
+    //     printf("Could not insert at given position\n");
+    // }
+    int value;
+    sout(&list);
+    // deleteNodeAtHead(&list, &value);
+    // deleteLast(&list, &value);
+    deleteNodeAtIndex(&list, 1, &value);
+    
     sout(&list);
 }
